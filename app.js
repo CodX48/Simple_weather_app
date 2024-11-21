2d8e7e880cb31790ff2e7f51da7df2a709380856
codx207 <codx207@gmail.com>
2024-11-21 15:17:56 +0300

    i add every thing i need as a style and elements but i did not start with the JS

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-// my API "a1374d9e7460474ea2c145924241911"
+// my APIKey "6a12c5971cfe1cea9fa803c16afe3b52"
+// my apiurl "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
