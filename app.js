e3570d94b877435f57f42806566d04580df3f533
codx207 <codx207@gmail.com>
2024-11-21 16:11:03 +0300

    I test the API And i added some functions for fetching

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-// my APIKey "6a12c5971cfe1cea9fa803c16afe3b52"
-// my apiurl "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}"
+const  APIKey = "6a12c5971cfe1cea9fa803c16afe3b52"
+
+async function getFeachData(city) {
+    return (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)).json()
+    }
+
+async function UpdateWeather(city) {
+    try {
+        const Data = await getFeachData(city);
+        console.log(Data)
+    }catch(err){
+        console.error(err)
+    }
+}
+
+UpdateWeather("istanbul")
+
