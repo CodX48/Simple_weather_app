20cd6b8c405e3edb346230eedc43f4d4f2cf467f
codx207 <codx207@gmail.com>
2024-11-22 00:04:29 +0300

    I add event listenters to the code so my code will be able to know what the user is searching for, returning the info from the api

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const  APIKey = "6a12c5971cfe1cea9fa803c16afe3b52"
+const search_input = document.querySelector('.search-input');
+const search_btn = document.getElementById('search_btn');
 
 async function getFeachData(city) {
     return (await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)).json()
     }
 
+    document.addEventListener('keydown',(Event)=>{
+        if(Event.key === 'Enter' && search_input.value != ''){ 
+          UpdateWeather(search_input.value);
+          Event.preventDefault();
+        }
+    });
+
+    search_btn.addEventListener('click' , (Event)=>{
+        if(search_input.value != ''){
+            UpdateWeather(search_input.value);
+            Event.preventDefault();
+        }
+    });
+
 async function UpdateWeather(city) {
     try {
         const Data = await getFeachData(city);
+        
         console.log(Data)
     }catch(err){
         console.error(err)
     }
 }
 
-UpdateWeather("istanbul")
+
 
